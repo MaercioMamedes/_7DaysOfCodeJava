5d233c7c6361b163783bc21f63c2717ba6341346
MaercioMamedes <maerciomamedes@hotmail.com>
2023-12-06 12:02:42 -0300

    second day finished

--- src/main/java/core/ApiConnection.java
+++ src/main/java/core/ApiConnection.java
@@ -8,14 +8,34 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ApiConnection {
     /**
      * urlBase para API fornecedora dos dados de filmes e sérias
      */
-    public final String urlBase = "https://api.themoviedb.org/3/authentication";
+
+    public static String urlImageBase = "https://image.tmdb.org/t/p/original";
+    public static final String urlBase = "https://api.themoviedb.org/3/authentication";
     public final String urlTopMovie = "https://api.themoviedb.org/3/movie/top_rated?language=pt-BR&page=";
+
+
+    public void connectionTestApi() throws IOException, InterruptedException {
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("webToken");
+        HttpRequest.Builder builder = HttpRequest.newBuilder();
+        builder.uri(URI.create(urlBase));
+        builder.header("accept", "application/json");
+        builder.header("Authorization", "Bearer " + token);
+        builder.method("GET", HttpRequest.BodyPublishers.noBody());
+        HttpRequest request = builder
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
 
     public JsonArray getListTopMovies(Integer page) throws IOException, InterruptedException {
         Dotenv dotenv = Dotenv.load();
@@ -35,14 +55,14 @@ public class ApiConnection {
 
     public void getListMovies(Integer topNumber) throws IOException, InterruptedException {
 
-        Integer count = 1;
+        int count = 1;
         Integer page = 0;
 
-        do{
+        do {
             page++;
 
             JsonArray listMovies = getListTopMovies(page);
-            for(JsonElement jsonElement: listMovies) {
+            for (JsonElement jsonElement : listMovies) {
 
                 Gson gson = new Gson();
                 if (count > topNumber) {
@@ -56,13 +76,63 @@ public class ApiConnection {
             }
 
 
-        }while (count <= topNumber);
-
-
+        } while (count <= topNumber);
 
     }
 
-    public void getMovieTop(){
+    public void getListImagesMoviesTop(int topNumber) throws IOException, InterruptedException {
+        int count = 1;
+        int page = 0;
+        List<String> listMoviesImages = new ArrayList<>();
+        do {
+            page++;
 
+            JsonArray listMovies = getListTopMovies(page);
+            for (JsonElement jsonElement : listMovies) {
+
+                if (count > topNumber) {
+                    break;
+                } else {
+                    JsonElement image_uri = jsonElement.getAsJsonObject().get("poster_path");
+                    String urlImage = urlImageBase + image_uri.toString().trim().replaceAll("^\"|\"$", "");
+                    listMoviesImages.add(urlImage);
+                    count++;
+                }
+            }
+
+
+        } while (count <= topNumber);
+
+        for (String movie : listMoviesImages) {
+            System.out.println(movie);
+        }
+    }
+
+    public void getTitlesMoviesTop(int topNumber) throws IOException, InterruptedException {
+        int count = 1;
+        int page = 0;
+        List<String> listMoviesTitles = new ArrayList<>();
+        do {
+            page++;
+
+            JsonArray listMovies = getListTopMovies(page);
+            for (JsonElement jsonElement : listMovies) {
+
+                if (count > topNumber) {
+                    break;
+                } else {
+                    JsonElement title = jsonElement.getAsJsonObject().get("title");
+                    listMoviesTitles.add(title.toString());
+                    count++;
+                }
+            }
+
+
+        } while (count <= topNumber);
+
+        for (String movie : listMoviesTitles) {
+            System.out.println(movie);
+        }
     }
 }
+
--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         ApiConnection api = new ApiConnection();
-        api.getListMovies(250);
+        api.connectionTestApi();
+        api.getListMovies(20);
+        api.getListImagesMoviesTop(21);
+        api.getTitlesMoviesTop(21);
     }
 }
